e597261469b06ea85f40ba968958748e0769af69
Amit Raj <harsh.harshraj.ra@gmail.com>
2026-01-07 00:22:01 +0530

    Refactor AllReviews block to use ToolsPanel for inspector controls

--- plugins/woocommerce/client/blocks/assets/js/blocks/reviews/all-reviews/block.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/reviews/all-reviews/block.tsx
@@ -3,7 +3,13 @@
  */
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import {
+	ToggleControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToolsPanel as ToolsPanel,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToolsPanelItem as ToolsPanelItem,
+} from '@wordpress/components';
 import { Icon, postComments } from '@wordpress/icons';
 
 /**
@@ -16,6 +22,21 @@ import {
 	getSharedReviewListControls,
 } from '../edit-utils.js';
 import type { AllReviewsEditorProps } from './types';
+
+const DEFAULT_ATTRIBUTES = {
+	showProductName: true,
+	showReviewRating: true,
+	showReviewerName: true,
+	showReviewImage: true,
+	showReviewDate: true,
+	showReviewContent: true,
+	imageType: 'reviewer',
+	showOrderby: true,
+	orderby: 'most-recent',
+	reviewsOnPageLoad: 10,
+	showLoadMore: true,
+	reviewsOnLoadMore: 10,
+};
 
 /**
  * Component to handle edit mode of "All Reviews".
@@ -31,24 +52,69 @@ const AllReviewsEditor = ( {
 	const getInspectorControls = () => {
 		return (
 			<InspectorControls key="inspector">
-				<PanelBody title={ __( 'Content', 'woocommerce' ) }>
-					<ToggleControl
+				<ToolsPanel
+					label={ __( 'Content', 'woocommerce' ) }
+					resetAll={ () =>
+						setAttributes( {
+							showProductName: DEFAULT_ATTRIBUTES.showProductName,
+							showReviewRating:
+								DEFAULT_ATTRIBUTES.showReviewRating,
+							showReviewerName:
+								DEFAULT_ATTRIBUTES.showReviewerName,
+							showReviewImage: DEFAULT_ATTRIBUTES.showReviewImage,
+							showReviewDate: DEFAULT_ATTRIBUTES.showReviewDate,
+							showReviewContent:
+								DEFAULT_ATTRIBUTES.showReviewContent,
+							imageType: DEFAULT_ATTRIBUTES.imageType,
+						} )
+					}
+				>
+					<ToolsPanelItem
 						label={ __( 'Product name', 'woocommerce' ) }
-						checked={ attributes.showProductName }
-						onChange={ () =>
+						hasValue={ () =>
+							attributes.showProductName !==
+							DEFAULT_ATTRIBUTES.showProductName
+						}
+						onDeselect={ () =>
 							setAttributes( {
-								showProductName: ! attributes.showProductName,
+								showProductName:
+									DEFAULT_ATTRIBUTES.showProductName,
 							} )
 						}
-					/>
+						isShownByDefault
+					>
+						<ToggleControl
+							label={ __( 'Product name', 'woocommerce' ) }
+							checked={ attributes.showProductName }
+							onChange={ () =>
+								setAttributes( {
+									showProductName:
+										! attributes.showProductName,
+								} )
+							}
+						/>
+					</ToolsPanelItem>
 					{ getSharedReviewContentControls(
 						attributes,
 						setAttributes
 					) }
-				</PanelBody>
-				<PanelBody title={ __( 'List Settings', 'woocommerce' ) }>
+				</ToolsPanel>
+				<ToolsPanel
+					label={ __( 'List Settings', 'woocommerce' ) }
+					resetAll={ () =>
+						setAttributes( {
+							showOrderby: DEFAULT_ATTRIBUTES.showOrderby,
+							orderby: DEFAULT_ATTRIBUTES.orderby,
+							reviewsOnPageLoad:
+								DEFAULT_ATTRIBUTES.reviewsOnPageLoad,
+							showLoadMore: DEFAULT_ATTRIBUTES.showLoadMore,
+							reviewsOnLoadMore:
+								DEFAULT_ATTRIBUTES.reviewsOnLoadMore,
+						} )
+					}
+				>
 					{ getSharedReviewListControls( attributes, setAttributes ) }
-				</PanelBody>
+				</ToolsPanel>
 			</InspectorControls>
 		);
 	};
--- plugins/woocommerce/client/blocks/assets/js/blocks/reviews/all-reviews/types.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/reviews/all-reviews/types.ts
@@ -1,8 +1,15 @@
+/**
+ * Internal dependencies
+ */
+import { SharedReviewAttributes } from '../types';
+
+interface AllReviewsAttributes extends SharedReviewAttributes {
+	showProductName: boolean;
+}
+
 export interface AllReviewsEditorProps {
-	attributes: {
-		showProductName: boolean;
-	};
-	setAttributes: ( attributes: { showProductName?: boolean } ) => void;
+	attributes: AllReviewsAttributes;
+	setAttributes: ( attributes: Partial< AllReviewsAttributes > ) => void;
 	debouncedSpeak: ( message: string ) => void;
 	idBase: unknown;
 	instance: {
